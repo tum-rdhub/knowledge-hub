ff78772c838a85a212b319b7d4e1e81f5e04cc89
Dilvan Sabir <42637035+ironicator@users.noreply.github.com>
2025-04-08 12:24:49 +0200

    Changed edit links to reflect "main" instead of "master"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/tum-rdhub/knowledge-hub/edit/master/',
+          editUrl: 'https://github.com/tum-rdhub/knowledge-hub/edit/main/',
 
           // editUrl:   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
