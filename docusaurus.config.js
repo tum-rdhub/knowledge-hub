9c8a6996ee3e578fa165e4eafa443deb5884c2ee
Dilvan Sabir <42637035+ironicator@users.noreply.github.com>
2025-04-08 13:15:20 +0200

    Removed "Edit this page"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,10 +46,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/tum-rdhub/knowledge-hub/edit/main/',
-
-          // editUrl:   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+          // editUrl: 'https://github.com/tum-rdhub/knowledge-hub/edit/main/',
+          },
         theme: {
           customCss: './src/css/custom.css',
         },
